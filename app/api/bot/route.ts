5b2e16ef4e6c82035a63ac80ede6b8b7d9595029
dechasaejara <dechasaejara@gmail.com>
2025-05-02 13:28:09 +0300

    update bot

--- app/api/bot/route.ts
+++ app/api/bot/route.ts
@@ -1,33 +1,25 @@
 import { Bot, webhookCallback } from "grammy";
 import { registerCommandHandlers } from "./handlers/commands";
 import { registerCallbackHandlers } from "./handlers/callbacks";
+import { NextRequest, NextResponse } from "next/server";
 
 const token = process.env.PRODUCTION_BOT_TOKEN as string;
 const ADMIN_ID = process.env.ADMIN_ID ? parseInt(process.env.ADMIN_ID) : null;
 
-if (!token)
+if (!token) {
   throw new Error("PRODUCTION_BOT_TOKEN environment variable not found.");
-if (!ADMIN_ID)
+}
+if (!ADMIN_ID) {
   throw new Error("ADMIN_ID environment variable not set or invalid.");
+}
 
-const bot = new Bot(token);
+ const bot = new Bot(token);
 
 // Register bot handlers
 registerCommandHandlers(bot, ADMIN_ID);
 registerCallbackHandlers(bot);
 
-export async function POST(request: Request) {
-  try {
-    const text = await request.text();
-    // Process the webhook payload
-    webhookCallback(bot, "std/http");
-  } catch (error) {
-    return new Response(`Webhook error: ${error}`, {
-      status: 400,
-    });
-  }
-
-  return new Response("Success!", {
-    status: 200,
-  });
-}
+// Create the webhook handler
+const endpoint = "https://josad-meet.onrender.com/api/bot"; // <-- put your URL here
+await bot.api.setWebhook(endpoint);
+export const POST = webhookCallback(bot, 'std/http')
